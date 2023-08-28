e591ab7afe919b780fc5b69b3c762bfe65f2afc4
Likhith Garapati <likhithgarapati2002@gmail.com>
2023-08-28 04:41:44 +0000

    FC3

--- src/main/java/com/knf/dev/demo/springboothelloworld/Application.java
+++ src/main/java/com/knf/dev/demo/springboothelloworld/Application.java
@@ -1,3 +1,8 @@
+package com.knf.dev.demo.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class Application {
 
