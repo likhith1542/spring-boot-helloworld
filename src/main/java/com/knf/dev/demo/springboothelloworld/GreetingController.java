89aab197bb0587e1e7fb4fb92c61bf048cc98b7e
Likhith Garapati <likhithgarapati2002@gmail.com>
2023-08-28 04:46:09 +0000

    FC5

--- src/main/java/com/knf/dev/demo/springboothelloworld/GreetingController.java
+++ src/main/java/com/knf/dev/demo/springboothelloworld/GreetingController.java
@@ -1,8 +1,6 @@
 package com.knf.dev.demo.springboothelloworld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.*;
 @RestController
 public class GreetingController {
 
